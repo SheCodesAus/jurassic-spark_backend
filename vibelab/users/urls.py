24a0f7db6ea0e534b5f287e1334e53234045b81b
Leanne Huang <leanne110x@gmail.com>
2025-12-04 22:52:16 +0800

    add username check api (#11)

--- vibelab/users/urls.py
+++ vibelab/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserRegistrationView, UserProfileView
+from .views import UserRegistrationView, UserProfileView, CheckUsernameView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-register'),
     path('profile/', UserProfileView.as_view(), name='user-profile'),
+    path('check-username/', CheckUsernameView.as_view(), name='check-username'),
 ]
